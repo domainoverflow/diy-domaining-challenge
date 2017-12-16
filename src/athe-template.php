11a3d65cbe70d8e9b0d377bbc3b98f258a166809
info@domainoverflow.com <info@domainoverflow.com>
2017-12-16 17:19:16 +0000

    template done

--- src/athe-template.php
+++ src/athe-template.php
@@ -5,31 +5,15 @@
 <html>
 
 
-<head>
+<header>
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-63552696-16"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-63552696-16');
-</script>
-
-
-
-  <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+  <script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
   integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
   crossorigin="anonymous"></script>
- 
-  <script type="text/javascript" src="https://e.ventures/content/more-balloons/moveobj.js"></script>  
- 
  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Tangerine">
 
- 
- 
-</head>
+</header>
 
     
 <style>
@@ -314,19 +298,7 @@ align-items: center;color:white;
   
   
 </div>
-  
-<DIV ID="flyimage1" STYLE="position:absolute; left: -500px; width:47; height:68;">
-<A HREF="https://domainoverflow.com" target="_blank"><IMG SRC="b1.gif"  BORDER=0></a>
-</DIV>
-
-<DIV ID="flyimage2" STYLE="position:absolute; left: -500px; width:47; height:68;">
-<A HREF="https://youtube.com/channel/UCsycucrfxx-ZDy63STch66g/videos" target="_blank"><IMG SRC="b2.gif" BORDER=0></a>
-</DIV>
-
-<DIV ID="flyimage3" STYLE="position:absolute; left: -500px; width:47; height:68;">
-<A HREF="https://domainoverflow.com" target="_blank"><IMG SRC="b3.gif" BORDER=0></a>
-</DIV>
-
+    
     </body>
 
    <script>
@@ -528,39 +500,14 @@ function UpdateTemplate () {
 
        
        
-
-       
-       
-    
  
-    
-  
-var flyimage1, flyimage2, flyimage3
-
-function pagestart(){
-//Step 2: Using the same variable names as 1), add or delete more of the below lines (60=width, height=80 of image):
- flyimage1=new Chip("flyimage1",47,68);
- flyimage2=new Chip("flyimage2",47,68);
- flyimage3=new Chip("flyimage3",47,68);
-
-
-//Step 3: Using the same variable names as 1), add or delete more of the below lines:
-movechip("flyimage1");
-movechip("flyimage2");
-movechip("flyimage3");
-
-}
-
-if (window.addEventListener)
-window.addEventListener("load", pagestart, false)
-else if (window.attachEvent)
-window.attachEvent("onload", pagestart)
-else if (document.getElementById)
-window.onload=pagestart
-
-</script>
    
        
+       
+       
+    
+</script> 
+    
     
     
 <footer></footer>
---
+++ src/moveobj.js
@@ -0,0 +1,77 @@
+/***********************************************
+* Floating image script- By Virtual_Max (http://www.geocities.com/siliconvalley/lakes/8620)
+* Modified by Dynamic Drive for various improvements
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+***********************************************/
+
+
+var vmin=2;
+var vmax=5;
+var vr=2;
+var timer1;
+
+function iecompattest(){
+return (document.compatMode && document.compatMode!="BackCompat")? document.documentElement : document.body
+}
+
+function Chip(chipname,width,height){
+ this.named=chipname;
+ this.vx=vmin+vmax*Math.random();
+ this.vy=vmin+vmax*Math.random();
+ this.w=width+20;
+ this.h=height;
+ this.xx=0;
+ this.yy=0;
+ this.timer1=null;
+}
+
+function movechip(chipname){
+if (document.getElementById){
+eval("chip="+chipname);
+   if (window.innerWidth || window.opera){
+		 pageX=window.pageXOffset;
+     pageW=window.innerWidth-40;
+     pageY=window.pageYOffset;
+     pageH=window.innerHeight-20;
+    }
+   else if (document.body){
+		 pageX=iecompattest().scrollLeft;
+     pageW=iecompattest().offsetWidth-40;
+     pageY=iecompattest().scrollTop;
+     pageH=iecompattest().offsetHeight-20;
+    } 
+
+   chip.xx=chip.xx+chip.vx;
+   chip.yy=chip.yy+chip.vy;
+   
+   chip.vx+=vr*(Math.random()-0.5);
+   chip.vy+=vr*(Math.random()-0.5);
+   if(chip.vx>(vmax+vmin))  chip.vx=(vmax+vmin)*2-chip.vx;
+   if(chip.vx<(-vmax-vmin)) chip.vx=(-vmax-vmin)*2-chip.vx;
+   if(chip.vy>(vmax+vmin))  chip.vy=(vmax+vmin)*2-chip.vy;
+   if(chip.vy<(-vmax-vmin)) chip.vy=(-vmax-vmin)*2-chip.vy;
+
+   if(chip.xx<=pageX){
+			chip.xx=pageX;
+      chip.vx=vmin+vmax*Math.random();
+     }
+   if(chip.xx>=pageX+pageW-chip.w){
+			chip.xx=pageX+pageW-chip.w;
+      chip.vx=-vmin-vmax*Math.random();
+     }
+   if(chip.yy<=pageY)
+     {chip.yy=pageY;
+      chip.vy=vmin+vmax*Math.random();
+     }
+   if(chip.yy>=pageY+pageH-chip.h)
+     {chip.yy=pageY+pageH-chip.h;
+      chip.vy=-vmin-vmax*Math.random();
+     }
+
+document.getElementById(chip.named).style.left=chip.xx+"px";
+document.getElementById(chip.named).style.top=chip.yy+"px";
+
+
+   chip.timer1=setTimeout("movechip('"+chip.named+"')",100);
+  }
+}
